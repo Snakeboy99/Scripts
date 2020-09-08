e0dd7b14c3197865becc9da67764d8ba94a915b3
YoungStar <54590731+Snakeboy99@users.noreply.github.com>
2020-09-08 12:09:41 -0300

    Update IslandFarmBerry.lua

--- IslandFarmBerry.lua
+++ IslandFarmBerry.lua
@@ -1,20 +1,29 @@
 local MyPlr = game:GetService('Players').LocalPlayer
 
 function FindIsland()
-    for i,v in pairs(workspace:GetChildren()) do
-        if v.Name == 'Islands' and v:IsA("Folder") then 
-            if v:FindFirstChild(tonumber(MyPlr.UserId) .. '-island') then 
-                return v:FindFirstChild(tonumber(MyPlr.UserId) .. '-island')
-            end
-        end
-    end
+   for i,v in pairs(workspace:GetChildren()) do
+       if v.Name == 'Islands' and v:IsA("Folder") then
+           if v:FindFirstChild(tonumber(MyPlr.UserId) .. '-island') then
+               return v:FindFirstChild(tonumber(MyPlr.UserId) .. '-island')
+           end
+       end
+   end
 end
 
-GetIsland = FindIsland()
-for _,v in pairs(GetIsland.Blocks:GetChildren()) do 
-    if v.Name == 'berryBush' and v.stage.Value ~= 1 then 
-        game:GetService('Players').LocalPlayer.Character:MoveTo(v.Position)
-        game.ReplicatedStorage.Remotes.Functions.CLIENT_HARVEST_CROP_REQUEST:InvokeServer({model = v, player = game:GetService('Players').LocalPlayer})
-        wait(.15)
+spawn(function()
+    while true do
+        BlocksFolder = FindIsland().Blocks
+        if getgenv().AutoFarmer and BlocksFolder then
+            for _,v in pairs(BlocksFolder:GetChildren()) do
+               if v.Name == 'berryBush' and v.stage.Value == 2 then
+                   if getgenv().AutoFarmer then
+                        game:GetService('Players').LocalPlayer.Character:MoveTo(v.Position)
+                        game.ReplicatedStorage.Remotes.Functions.CLIENT_HARVEST_CROP_REQUEST:InvokeServer({model = v, player = game:GetService('Players').LocalPlayer})
+                        wait(.15)
+                   end
+               end
+            end
+        end
+        wait(2)
     end
-end
+end)
